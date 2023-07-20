f3599c972cf99f2ebcaee190eb29a48c51c55a8f
Pac12Run8000 <gangstarr78992@>
2023-07-20 13:19:57 -0700

    Initial Commit

---
+++ Snackbar/SnackbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackbarApp.swift
+//  Snackbar
+//
+//  Created by Michelle Grover on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SnackbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
