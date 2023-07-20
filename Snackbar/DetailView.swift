6e2ea1e203385b0d2784aaa467b63b485c9b19e0
Pac12Run8000 <gangstarr78992@>
2023-07-20 13:49:12 -0700

    SnackBar is working

---
+++ Snackbar/DetailView.swift
@@ -0,0 +1,16 @@
+//
+//  DetailView.swift
+//  Snackbar
+//
+//  Created by Michelle Grover on 7/20/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("Detail")
+        }
+    }
+}
---
+++ Snackbar/Views/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Snackbar
+//
+//  Created by Michelle Grover on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showSnackbar = false
+    @State private var navigateToDetail = false
+
+    var body: some View {
+        VStack {
+            NavigationView {
+                        VStack {
+                            Button(action: {
+                                navigateToDetail = true
+                            }) {
+                                Text("Go there")
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            NavigationLink(destination: DetailView(), isActive: $navigateToDetail) {
+                                EmptyView()
+                            }
+                            Spacer()
+                        }
+                        .navigationBarTitle("DetailView", displayMode: .inline)
+                    }
+            
+            Button("Show Snackbar") {
+                withAnimation {
+                    showSnackbar = true
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        showSnackbar = false
+                    }
+                }
+            }
+
+            Spacer()
+
+            if showSnackbar {
+                Text("This is a snackbar message")
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    .transition(.move(edge: .bottom))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
